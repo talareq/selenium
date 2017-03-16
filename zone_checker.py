49f5a900f86056933b1fd81ba34ad2b4f09864a4
talareq <galuszka.grzegorz@yahoo.com>
2017-03-16 15:28:52 +0000

    Homework 9
    Checking if zones are in alphabetical order

--- zone_checker.py
+++ zone_checker.py
@@ -23,30 +23,28 @@ def test_example(driver):
     geo_zones=get_countries_list(geo_zones_table)
     geo_zones_sorted=alphabetical(geo_zones_table)
     assert geo_zones == geo_zones_sorted
+    driver.find_element_by_css_selector(
+        'a[href="http://localhost/litecart/admin/?app=countries&doc=countries"]').click()
 
-    for row in table:
+    for row in driver.find_elements_by_css_selector("tr.row"):
 
         cells=row.find_elements_by_tag_name("td")
         zone=cells[5].text
 
-        for zone != 0:
+        if zone != str(0):
             cells = row.find_elements_by_tag_name("td")
             cells[4].click
-            country_list=driver.find_element_by_id("table-zones")
-            zone_countries=get_countries_list(country_list)
-            sorted_zone_countries=alphabetical(zone_countries)
-            assert zone_countries == sorted_zone_countries
+            country_list=driver.find_elements_by_css_selector(".dataTable")
+            countries = []
+            for row in country_list:
+                cells = row.find_elements_by_tag_name("tr")
+                name = cells[2].text
+                countries.append(name)
+                return countries
+            sorted_zone_countries=alphabetical(countries)
+            assert countries == sorted_zone_countries
             driver.find_element_by_css_selector(
                 'a[href="http://localhost/litecart/admin/?app=countries&doc=countries"]').click()
-
-
-
-
-
-
-
-
-
 
 
 
