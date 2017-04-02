302804aa34149ea38b42fe7b55d806211e9e4435
talareq <galuszka.grzegorz@yahoo.com>
2017-04-02 16:46:25 +0100

    Homework 17
    Get Webbrowser logs

--- test/log_check.py
+++ test/log_check.py
@@ -1,11 +1,8 @@
 
 
 def test_example(app):
-    app.get(" http://localhost/litecart/admin/")
-    app.driver.find_element_by_name("username").send_keys("admin")
-    app.driver.find_element_by_name("password").send_keys("admin")
-    app.driver.find_element_by_name("login").click()
-    app.driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1")
+    app.login_admin()
+    app.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1")
     menu=app.driver.find_elements_by_css_selector("tr .row")
     for n in range(0,len(menu)):
         element = app.driver.find_elements_by_css_selector("tr .row")
